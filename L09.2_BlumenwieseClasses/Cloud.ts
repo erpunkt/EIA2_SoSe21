4f832c07aa78f0ec3e07605620cf44a998f84d0e
erpunkt <rebecca.rachel.raeschke@hs-furtwangen.de>
2021-05-29 18:53:29 +0200

    L09.2

--- L09.2_BlumenwieseClasses/Cloud.ts
+++ L09.2_BlumenwieseClasses/Cloud.ts
@@ -26,15 +26,15 @@ namespace flowerClasses {
         
         if (this.position.x < 0)
         this.position.x += crc2.canvas.width;
-        
-
+    
         
         } 
 
 draw(): void {
-    crc2.fillStyle = "white";
+    
     crc2.beginPath();
     crc2.arc(this.position.x + this.size, this.position.y, this.size, 0, 2 * Math.PI);
+    crc2.fillStyle = "white";
     crc2.fill();
     crc2.closePath();
 
